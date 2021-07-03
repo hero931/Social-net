b3cb64f1894cc8bf153ccf57d39033be5a8cff18
Ilya Andreev <31460531+hero931@users.noreply.github.com>
2021-07-03 16:16:04 -0400

    logout have been added

--- includes/header.php
+++ includes/header.php
@@ -27,16 +27,6 @@
 <nav class="navbar navbar-expand-lg py-3 fixed-top">
       <div class="container">
         <a href="index.php" class="navbar-brand">MyNet</a>
-
-        <!-- <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navmenu"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button> -->
-
         <div class="collapse navbar-collapse" id="navmenu">
           <ul class="navbar-nav ms-auto">
             <li class="nav-item">
@@ -47,6 +37,9 @@
             </li>
             <li class="nav-item">
               <a href="#" class="nav-link"><i class="far fa-home"></i></a>
+            </li>
+            <li class="nav-item">
+              <a href="includes/handlers/logout.php" class="nav-link"><i class="far fa-sign-out-alt"></i></a>
             </li>
           </ul>
         </div>

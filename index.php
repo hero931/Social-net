6becc4b7d2b6f0abe8ccd3fab80da2a54f571340
Ilya Andreev <31460531+hero931@users.noreply.github.com>
2021-07-03 15:02:40 -0400

    post submit updated

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php 
     include("includes/header.php");
     include("includes/classes/User.php");
+    include("includes/classes/Post.php");
+
+    if(isset($_POST['post'])) {
+        $post = new Post($con, $userLoggedIn);
+        $post->submitPost($_POST['post_text'], 'none');
+    }
 ?>
 
     
@@ -24,36 +30,18 @@
                             </div>
                         </div>
                 </div>
-            </div>
+            </div>            
             
-            <div class="card mb-3">
-                <div class="row g-0">
-                        <div class="col m-4">
-                            <img src="<?php echo $user['profile_pic']; ?>" class="rounded-circle mb-3" alt="image" class="img-fluid rounded-start" alt="...">
-                        </div>
-                        <div class="col m-2 p-3">
-                            <div class="card-body">
-                            <h5 class="card-title mb-3">
-                                <a href="#">
-                                    <?php echo $user['first_name'] . " " . $user['last_name']; ?>
-                                </a>                                    
-                            </h5>                        
-                                <p class="card-text"><small class="text-muted"><i class="far fa-post"><?php echo "Posts: " . $user['num_posts']; ?></i></small></p>
-                                <p class="card-text"><small class="text-muted"><i class="far fa-thumbs-up"><?php echo ": " . $user['num_likes']; ?></i></small></p>
-                            </div>
-                        </div>
-                </div>
-            </div>
         </div>                           
         <div class="col-sm-6">
             <div class="card bg-light">
                 <div class="card-body text-center">
                 <form class="post_form flex-row" action="index.php" method="POST">
                     <div class="mb-6">                        
-                        <textarea type="email" class="form-control" placeholder="Got something to say?"  aria-describedby="post"></textarea>                                                
+                        <textarea name="post_text" id="post_text" class="form-control" placeholder="Got something to say?"  aria-describedby="post"></textarea>                                                
                     </div> 
                     <hr>                   
-                    <button type="submit" id="post" class="btn btn-outline-secondary">Post</button>                    
+                    <button type="submit" name="post" id="post" class="btn btn-outline-secondary">Post</button>                    
                 </form>
                 <?php $user_obj = new User($con, $userLoggedIn);
                     echo $user_obj->getFirstAndLastName();
